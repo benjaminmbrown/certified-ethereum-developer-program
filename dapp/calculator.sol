946c604ccf2993bbecacc4139569b0a183144667
Benjamin Brown <benjaminmbrown@Benjamins-MacBook-Air-2.local>
2017-12-09 15:27:10 -0500

    Add first solidity contract

---
+++ dapp/calculator.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.0;
+
+contract Calculator {
+    uint addResult;
+
+    function addNumber (uint n1, uint n2){
+        addResult = n1 + n2;
+    }
+
+    function getResult() constant returns (uint) {
+        return addResults;
+    }
+}
